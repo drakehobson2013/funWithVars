a2b5e6e0572da3090ea4c81b65581f469836f319
Drake Hobson <drakehobson2013@gmail.com>
2021-01-21 09:26:39 -0500

    push it

---
+++ test.js
@@ -0,0 +1,24 @@
+var myVar1 = 1
+var myVar2 = 2
+var myVar3 = 3
+var myVar4 = 4
+var myVar5 = 5
+var myVar6 = 6
+var myVar7 = 7
+var myVar8 = 8
+var myVar9 = 9
+var myVar10 = 10
+
+console.log(myVar1 + myVar2);
+console.log(myVar2 + myVar3);
+console.log(myVar5 - myVar4);
+console.log(myVar6 - myVar5);
+console.log(myVar7 * myVar8);
+console.log(myVar9 * myVar10);
+console.log(myVar10 / myVar2);
+console.log(myVar9 / myVar3);
+console.log(++myVar9);
+console.log(++myVar7);
+console.log(--myVar3);
+console.log(--myVar8);
+
